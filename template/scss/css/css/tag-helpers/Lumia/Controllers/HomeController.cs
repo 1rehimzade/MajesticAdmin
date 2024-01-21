5dae6772af33e7b1e4d47821f17db78b00fcb0e4
nihatab104 <146129654+1rehimzade@users.noreply.github.com>
2024-01-21 17:31:53 +0400

    admin

---
+++ template/scss/css/css/tag-helpers/Lumia/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Lumia.DAL;
+using Lumia.Models;
+using Lumia.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lumia.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ICollection<Product> products = await _context.Products.Include(x => x.Category).ToListAsync();
+            ICollection<Service> services = await _context.Services.ToListAsync();
+
+            HomeVM vM = new HomeVM { Products = products, Services = services };
+            return View(vM);
+        }
+    }
+}
